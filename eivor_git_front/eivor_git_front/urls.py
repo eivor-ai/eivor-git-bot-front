c20fec59935b773537baf88f552fe0e212e15d47
Jay Godara <jgodara@sarvika.com>
2019-02-05 08:06:15 -0800

    Added basic react

--- eivor_git_front/eivor_git_front/urls.py
+++ eivor_git_front/eivor_git_front/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
+    path('', include('frontend.urls')),
     path('', include('api.urls')),
     path('api/token/', TokenObtainPairView.as_view()),
     path('api/token/refresh', TokenRefreshView.as_view())
---
+++ eivor_git_front/frontend/src/components/integrations/Integrations.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Integrations extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Your integrations</h1>
+      </div>
+    )
+  }
+}
+
+export default Integrations
